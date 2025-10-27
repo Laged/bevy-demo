af4063b58cc53348a7680835b0b31f695741f3de
Laged <parkkila.matti@gmail.com>
2025-10-27 11:31:03 +0200

    feat: implement state transition helpers (set_state, get_state)

--- src/test_utils/simulation.rs
+++ src/test_utils/simulation.rs
@@ -17,17 +17,27 @@ pub fn run_frames(app: &mut App, num_frames: usize) {
 
 /// Transitions the app to the specified game state and runs one frame.
 ///
+/// This inserts a `NextState<S>` resource to trigger the state transition,
+/// then runs one update to apply the transition.
+///
 /// # Example
 /// ```ignore
 /// set_state(&mut app, GameState::InGame);
 /// ```
 pub fn set_state<S: States>(app: &mut App, state: S) {
-    // TODO: Implement in task 9
-    todo!("Will be implemented in task 9")
+    app.world_mut().insert_resource(NextState::Pending(state));
+    app.update();
 }
 
 /// Gets the current game state from the app.
+///
+/// Returns None if the state resource doesn't exist.
+///
+/// # Example
+/// ```ignore
+/// let current = get_state::<GameState>(&app);
+/// assert_eq!(current, Some(GameState::InGame));
+/// ```
 pub fn get_state<S: States + Copy>(app: &App) -> Option<S> {
-    // TODO: Implement in task 9
-    todo!("Will be implemented in task 9")
+    app.world().get_resource::<State<S>>().map(|s| s.get().clone())
 }
